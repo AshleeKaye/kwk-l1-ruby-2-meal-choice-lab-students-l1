a3d4c7b393287a62c637cf3aa9b429f6a15e08ca
AshleeKaye <ashleemk@hotmail.com>
2018-06-05 17:30:18 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -4,9 +4,10 @@ def snacks(food="Cheetos")
 end
 
 # Define breakfast, lunch and dinner methods that return the meal choice passed into them with a default to your favorite.
-def meals (#{breakfast}, #{lunch}, #{dinner})
-puts 
-
+def meals(breakfast, lunch, dinner)
+  puts "#{breakfast} is the best in the morning #{lunch} is best in the afternoon and #{dinner} is best at night"
+end
+meals("Pancakes", "sandwiches", "steak")
 
 # Call the methods with puts and your own arguments here. Like this:
 puts snacks("Swedish fish")
